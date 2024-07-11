a520ca49c01963b6d506b1278046403a4124cef7
Samuel Quintero Solís <160605624+samuelquisol@users.noreply.github.com>
2024-07-11 18:40:17 -0500

    Revert "Feat/modules assembly to Dev"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,35 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import dbConfig from './libs/persistence/mongodb/config/db-config';
-import { PersistenceModule } from './libs/persistence/persistence.module';
-import { APP_FILTER } from '@nestjs/core';
-import { HttpExceptionFilter } from './libs/common/filters/http-exception.filter';
-import { MailModule } from './mail/mail.module';
-import { CronModule } from './cron/cron.module';
-import { UsersModule } from './queries/users/users.module';
-import { TipsModule } from './queries/tips/tips.module';
-import { CronJobsModule } from './queries/cron-jobs/cron-jobs.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      load: [dbConfig],
-      isGlobal: true,
-    }),
-    PersistenceModule,
-    MailModule,
-    CronModule,
-    UsersModule,
-    TipsModule,
-    CronJobsModule,
-  ],
-  controllers: [],
-  providers: [
-    {
-      provide: APP_FILTER,
-      useClass: HttpExceptionFilter,
-    },
-  ],
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/libs/persistence/mongodb/config/db-config.ts
+++
@@ -1,8 +0,0 @@
-import { registerAs } from '@nestjs/config';
-
-export default registerAs('dbConfig', () => {
-  const dbConfig = {
-    db: process.env.URI_DB,
-  };
-  return dbConfig;
-});
