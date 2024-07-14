a0862eafdcd66073bf8a7dd017aa593492cdd1e9
Andres Gomez <pipe2893@gmail.com>
2024-07-13 22:52:18 -0500

    update swagger documentation

--- src/bots-module/controllers/bots.controller.ts
+++ src/bots-module/controllers/bots.controller.ts
@@ -1,18 +1,29 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
 import { BotService } from '../services/bots.service';
 import { TipDto } from '../dtos/tip.dto';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiTags, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 
 @ApiTags('Bots')
 @Controller('bots')
 export class BotsController {
   constructor(private readonly botService: BotService) {}
 
-  @Post('send-bots-tip')
+  @Post('send-tip')
   @ApiOperation({ summary: 'Send a tip to bots' })
   @ApiResponse({ status: 201, description: 'Tip sent successfully.' })
   @ApiResponse({ status: 400, description: 'Invalid input.' })
-  async sendTipToBots(@Body() tipDto: TipDto): Promise<void> {
-    await this.botService.sendTipToBots(tipDto);
+  @ApiResponse({ status: 500, description: 'Internal Server Error.' })
+  @ApiBody({
+    description: 'Details of the tip to be sent to bots',
+    type: TipDto,
+  })
+  async sendTipToBots(@Body() tipDto: TipDto) {
+    try {
+      await this.botService.sendTipToBots(tipDto);
+      return { statusCode: 201, message: 'Tip sent successfully.' };
+    } catch (error) {
+      throw new HttpException('Internal Server Error', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 }
+
